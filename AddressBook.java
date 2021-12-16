7b4696bd0cedd172f1c144e30ca7ec179be2f1a7
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-16 17:51:56 +0530

    UC9Address

--- AddressBook.java
+++ AddressBook.java
@@ -10,7 +10,6 @@ import javax.swing.*;
 class Person {
 
 	String First_name, Last_name, address, phoneNum, zip, city, state, email;
-	public Object name;
 
 	public Person(String F_name, String L_name, String Addr, String MobileNum, String ZipCode, String City,
 			String State, String Mail) {
@@ -33,6 +32,115 @@ class Person {
 	}
 }
 
+class Addressbook1 {
+
+	ArrayList<Person> P_list = new ArrayList<>();
+
+	/*
+	 * UC2
+	 */
+	public void AddPreson() {
+
+		String First_name = JOptionPane.showInputDialog("Enter First name");
+		String Last_name = JOptionPane.showInputDialog("Enter Last name");
+		String address = JOptionPane.showInputDialog("Enter address");
+		String phoneNum = JOptionPane.showInputDialog("Enter phone number");
+		String zip = JOptionPane.showInputDialog("Enter zip");
+		String city = JOptionPane.showInputDialog("Enter city");
+		String state = JOptionPane.showInputDialog("Enter state");
+		String email = JOptionPane.showInputDialog("Enter Mail");
+
+		Person data = new Person(First_name, Last_name, address, phoneNum, zip, city, state, email);
+
+		// add the above PersonInfo object to arraylist
+		P_list.add(data);
+		P_list.toString();
+//		for (Person p : P_list) {
+//			p.print();
+//		}
+	}
+
+	/*
+	 * UC3
+	 */
+	public void EditePeson(String name, String name1) {
+		for (Person i : P_list) {
+			if (name.equals(i.First_name)) {
+				i.First_name = name1;
+				i.print();
+			}
+
+		}
+	}
+
+	/*
+	 * UC4
+	 */
+	public void DeletePerson(String name) {
+		for (int i = 0; i < P_list.size(); i++) {
+			Person p = P_list.get(i);
+			if (name.equals(p.First_name)) {
+				P_list.remove(i);
+				System.out.println("Person got deleted");
+				System.out.println(P_list);
+			} else
+				System.out.println("Person with this given name not found!!!!!");
+		}
+	}
+
+	/*
+	 * UC5 Ability to add multiple person to Address Book
+	 */
+	public void AddMultiPerson() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many person you want to add :");
+		int Count = sc.nextInt();
+		for (int i = 1; i <= Count; i++) {
+			AddPreson();
+		}
+		for (Person p : P_list) {
+			p.print();
+		}
+	}
+}
+
+/*
+ * UC6
+ */
+class AddressHashMap {
+	HashMap<String, Person> map = new HashMap<>();
+
+//	public static HashMap<String, ArrayList<ContactPerson>>
+	public void AddPresonHashmap() {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many person you want to add :");
+		int Count = sc.nextInt();
+		for (int i = 1; i <= Count; i++) {
+			String First_name = JOptionPane.showInputDialog("Enter First name");
+			String Last_name = JOptionPane.showInputDialog("Enter Last name");
+			String address = JOptionPane.showInputDialog("Enter address");
+			String phoneNum = JOptionPane.showInputDialog("Enter phone number");
+			String zip = JOptionPane.showInputDialog("Enter zip");
+			String city = JOptionPane.showInputDialog("Enter city");
+			String state = JOptionPane.showInputDialog("Enter state");
+			String email = JOptionPane.showInputDialog("Enter Mail");
+
+			Person data = new Person(First_name, Last_name, address, phoneNum, zip, city, state, email);
+
+			// add the above PersonInfo object to arraylist
+			map.put(email, data);
+		}
+		printmap(map);
+	}
+
+	public void printmap(HashMap<String, Person> map) {
+		for (String map1 : map.keySet()) {
+			System.out.println("Key: " + map1 + " Value: " + map.get(map1));
+		}
+	}
+}
 
 class AddressBook {
 	public static void main(String[] args) {
@@ -42,15 +150,18 @@ class AddressBook {
 		 * UC1 Ability to create a Contacts in Address Book with first and last names,
 		 * address, city, state, zip, phone number and email...
 		 */
-			Person s1 = new Person("Ashwath", "Naidu", "123ash,nea rsai baba temple road,", "934058XXX", "560037",
-					"Bangalour", "Karnataka", "Ashwath@xyz.in");
-			s1.print();
+//			Person s1 = new Person("Ashwath", "Naidu", "123ash,nea rsai baba temple road,", "934058XXX", "560037",
+//					"Bangalour", "Karnataka", "Ashwath@xyz.in");
+//			s1.print();
 
-//		Addressbook1 b = new Addressbook1();
-////		b.AddPreson(); // first person
-//		b.AddPreson(); // second person
-//		b.EditePeson("ashwath", "naidu");
-//		b.DeletePerson("naidu");
+		Addressbook1 b = new Addressbook1();
+//		b.AddPreson(); // first person// UC2
+//		b.AddPreson(); // second person // UC2
+//		b.EditePeson("ashwath", "naidu"); // UC3
+//		b.DeletePerson("naidu"); // UC4
+//		b.AddMultiPerson(); //UC5
 
+		AddressHashMap n = new AddressHashMap();
+		n.AddPresonHashmap();// UC6
 	}
 }
